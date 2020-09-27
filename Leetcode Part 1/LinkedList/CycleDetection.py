34995378ed6ae7af63129163e6020fc57d24a68a
Daniel Bae <danieljbaee@hotmail.com>
2020-09-26 22:23:22 -0700

    9/26 update

---
+++ Leetcode Part 1/LinkedList/CycleDetection.py
@@ -0,0 +1,25 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def hasCycle(self, head):
+        """
+        :type head: ListNode
+        :rtype: bool
+        """
+        if not head or not head.next:
+            return False
+        
+        slow = head.next
+        fast = head.next.next
+        
+        while slow != fast:
+            if (not fast) or (not fast.next): # if slow or fast is null, then no cycle
+                return False
+            slow = slow.next
+            fast = fast.next.next
+        
+        return True
---
+++ Leetcode Part 1/LinkedList/ReverseLinkedList.py
@@ -0,0 +1,23 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def reverseList(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        # Recursive approach - O(n) time and O(n) space
+        
+        if (not head) or (not head.next): # end of linked list
+            return head
+        
+        rev_head = self.reverseList(head.next) # recurse - downwards on stack  
+        head.next.next = head # reversal - logic: next node will now point to current node
+        head.next = None # remove existing link, such that linkedlist remains singly linked
+        return rev_head # return recursion - upwards stack 
+        
+            
+        
